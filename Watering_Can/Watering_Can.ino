f10cd3dd8467d515b481a1b56e58b90e78ca6108
Arthur Skinner <arthurskinner1997@gmail.com>
2021-02-06 16:36:42 +0000

    Tidied Arduino code
    Cleared old lines of code and added comments

--- Watering_Can/Watering_Can.ino
+++ Watering_Can/Watering_Can.ino
@@ -1,5 +1,5 @@
-int lowest = 200;
-int highest = 600;
+int lowest = 200;                 // enter the highest value received in data
+int highest = 600;                // enter the lowest value received in data
 int range = (highest - lowest);
 
 void setup()
@@ -10,7 +10,7 @@ void setup()
 
     void loop()
 {
-  // Read the input on analog pin 0:
+  // Read the input on analog pins:
   int sensorValue1 = analogRead(A0);
   int sensorValue2 = analogRead(A1);
   int sensorValue3 = analogRead(A2);
@@ -18,40 +18,15 @@ void setup()
   int sensorValue5 = analogRead(A4);
 
 
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage to a percentage of the range:
   float Sensor1 = ((float)(100-(((float)( sensorValue1-lowest)/range)*100)));
   float Sensor2 = ((float)(100-(((float)(sensorValue2-lowest)/range)*100)));
   float Sensor3 = ((float)(100-(((float)(sensorValue3-lowest)/range)*100)));
   float Sensor4 = ((float)(100-(((float)(sensorValue4-lowest)/range)*100)));
   float Sensor5 = ((float)(100-(((float)(sensorValue5-lowest)/range)*100)));
 
- 
-  // Print out the value you read:
-
-
 {
-//  Serial.print(sensorValue1);
-//  Serial.print(" RawOnion ");
-//  Serial.print(sensorValue2);
-//  Serial.print(" RawRadishNew ");
-//  Serial.print(sensorValue3);
-//  Serial.print(" RawChives ");
-//  Serial.print(sensorValue4);
-//  Serial.print(" RawChives ");
-//  Serial.print(sensorValue5);
-//  Serial.print(" RawRadish ");
-//
-//  Serial.print(Sensor5);
-//  Serial.print(" Radish ");
-//  Serial.print(Sensor4);
-//  Serial.print(" Chives ");
-//  Serial.print(Sensor3);
-//  Serial.print(" Chives ");
-//  Serial.print(Sensor2);
-//  Serial.print(" NewRadishes ");
-//  Serial.print(Sensor1);
-//  Serial.print(" Onions ");
-
+  // Print out the percentage values in line format to serial:
 Serial.print("moisture,batch=one");
 
 Serial.print(" Sensor5=");
@@ -72,13 +47,10 @@ Serial.print(",");
 
 Serial.print("Sensor1=");
 Serial.print(Sensor1);
-//Serial.print(",");
-  
- 
+
   Serial.println();
 }
 
-   
   // Wait 100 milliseconds
   delay(100);
 }
